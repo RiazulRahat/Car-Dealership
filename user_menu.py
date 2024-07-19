2b08d8cee83484fe8d7acfa10247a01cbfa2a3a5
RiazulRahat <85854559+RiazulRahat@users.noreply.github.com>
2024-07-18 21:49:57 -0400

    Updated user_menu.py
    Added View Cars functionality

--- user_menu.py
+++ user_menu.py
@@ -23,34 +23,44 @@ def connect():
             print('Database connection terminated.')
 
 def userLoginMenu():
-    print("Welcome To The Car Dealership!")
-    print("1. Login")
-    print("2. Sign Up")
+    while True:
+        print("Welcome To The Car Dealership!")
+        print("1. Login")
+        print("2. Sign Up")
+        print("3. Exit")
 
-    num = int(input())
+        num = int(input())
 
-    if (num == 1):
-        userLogin()
-    elif (num == 2):
-        userSignup()
-    else:
-        print("Wrong Entry!! Try Again!!")
-        userLoginMenu()
+        if (num == 1):
+            userLogin()
+        elif (num == 2):
+            userSignup()
+        elif (num == 3):
+            print("Exiting program. ")
+            break
+        else:
+            print("Wrong Entry!! Try Again!!")
     
 
 def userMain_menu():
-    print("MAIN MENU: ")
-    print("1. View Available Cars")
-    print("2. Contact Dealership")
+    while True:
+        print("MAIN MENU: ")
+        print("1. View Available Cars")
+        print("2. Contact Dealership")
+        print("3. Logout")
 
-    num = int(input())
-    if (num == 1):
-        pass # Data to show available cars
-    elif (num == 2):
-        pass # Data to contact Dealership
-    else:
-        print("Wrong Entry!! Try Again!!")
-        userMain_menu()
+        num = int(input())
+        if (num == 1):
+            # Data to show available cars
+            viewAvailableCars()
+
+        elif (num == 2):
+            pass # Data to contact Dealership
+        elif (num == 3):
+            print("Logging out.")
+            break
+        else:
+            print("Wrong Entry!! Try Again!!")
 
 
 def userLogin():
@@ -84,10 +94,17 @@ def userLogin():
 
 
 def userSignup():
-    fName = input("Enter First Name: ")
-    lName = input("Enter Last Name: ")
-    username = input("Enter Your Username: ")
-    password = input("Enter Password: ")
+    while True:
+        fName = input("Enter First Name: ").strip()
+        lName = input("Enter Last Name: ").strip()
+        username = input("Enter Your Username: ").strip()
+        password = input("Enter Password: ").strip()
+
+        if not fName or not lName or not username or not password:
+                print("All fields are required. Please try again.")
+        else:
+            break
+
     # Add data to database
     connection = None
     try:
@@ -106,7 +123,32 @@ def userSignup():
         if connection is not None:
             connection.close()
 
-
+def viewAvailableCars():
+    connection = None
+    try:
+        params = config()
+        connection = psycopg2.connect(**params)
+        crsr = connection.cursor()
+        
+        crsr.execute("SELECT year, make, model, mileage, amount FROM cars")
+        cars = crsr.fetchall()
+        
+        if cars:
+            print("Available Cars:")
+            print("[] - table column name")
+            for idx, car in enumerate(cars, start=1):
+                print(f"{idx}. {car[0]} {car[1]} {car[2]} - mileage: {car[3]} - Buy price: {car[4]}")
+        else:
+            print("No available cars.")
+        
+        crsr.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection is not None:
+            connection.close()
+        
+    input("Press Enter to return to the main menu...")
 
 if __name__=="__main__":
     connect()
